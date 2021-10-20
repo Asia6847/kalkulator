a734401f1ef7840c9f0fd64d857b79b00ce44054
Your Name <you@example.com>
2021-10-20 22:34:38 +0200

    dodane więcej operacji

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,36 @@ public class Main {
         int a= sc.nextInt();
         System.out.print("Wpisz drugą liczbę");
         int b= sc.nextInt();
-        int c= a + b;
-        System.out.print("Wynik to " + c);
+        System.out.print("Podaj operację");
+        String operator = sc.next();
+        System.out.print(operator);
+        int result = 0;
+//        if (operator.equals("+")) {
+//            result = a + b;
+//        }
+//        if (operator.equals("-")) {
+//            result = a - b;
+//        }
+//        if (operator.equals("/")) {
+//            result = a/b;
+//        }
+//        if (operator.equals("*")) {
+//            result = a*b;
+//        }
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "/":
+                result = a/b;
+                break;
+            case "*":
+                result = a*b;
+        }
 
-        System.out.print("AAA");
+        System.out.print("Wynik to " + result);
     }
 }
