3d76c13f2caa054d446d27560da7d1d4e7f01301
Your Name <you@example.com>
2021-10-20 21:53:12 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Wpisz liczbę");
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        System.out.print("Wpisz drugą liczbę");
+        int b= sc.nextInt();
+        int c= a + b;
+        System.out.print("Wynik to " + c);
+
+
+
+
+
+
+
+    }
+}
